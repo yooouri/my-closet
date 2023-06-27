118401eb8c7007f2fa493093fb06b9f483b49771
yoooouri <yurikim@sgacorp.kr>
2023-06-27 22:31:22 +0900

    storyboard remove setting

--- MyCloset/ViewController.swift
+++ MyCloset/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setting()
     }
-
+    func setting() {
+        view.backgroundColor = .white
+        
+        label.text = "test입니다"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
+        navigationController?.navigationBar.tintColor = .blue
+    }
 
 }
 
